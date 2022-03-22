7c21ea2ff063f7fac181cc37c7016f2903cc1af4
lzw <1922623640@qq.com>
2022-03-22 13:24:27 +0800

    第二次提交

--- 002-springboot-two/src/main/java/com/bjpowernode/hello.java
+++ 002-springboot-two/src/main/java/com/bjpowernode/hello.java
@@ -8,5 +8,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("测试Git");
         System.out.println("hello Git");
+        System.out.println("hahahhahah");
     }
 }
