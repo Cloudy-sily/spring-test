65a803b8baaec4be1fa05eaed5999374c5b48759
lzw <1922623640@qq.com>
2022-03-22 13:22:20 +0800

    第一次提交

---
+++ 002-springboot-two/src/main/java/com/bjpowernode/hello.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+/**
+ * @author shkstart
+ * @create 2022-03-22 13:01
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("测试Git");
+        System.out.println("hello Git");
+    }
+}
